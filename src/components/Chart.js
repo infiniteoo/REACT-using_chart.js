a1246720dcbf2fddc88b13b514e9fd169f4833eb
Troy Dorman <troydorman@gmail.com>
2021-07-25 15:56:55 -0600

    fixed bug in code, now chart is displaying successfully

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -6,37 +6,32 @@ export const Chart = () => {
     chartData: {
       labels: [
         "Boston",
-        "Worchester",
+        "Worcester",
         "Springfield",
         "Lowell",
         "Cambridge",
         "New Bedford",
       ],
+      datasets: [
+        {
+          label: "Population",
+          data: [617594, 181045, 153060, 106519, 105162, 95072],
+          backgroundColor: [
+            "rgba(255, 99, 132, 0.6)",
+            "rgba(54, 162, 235, 0.6)",
+            "rgba(255, 206, 86, 0.6)",
+            "rgba(75, 192, 192, 0.6)",
+            "rgba(153, 102, 255, 0.6)",
+            "rgba(255, 159, 64, 0.6)",
+            "rgba(255, 99, 132, 0.6)",
+          ],
+        },
+      ],
     },
-    datasets: [
-      {
-        label: "Population",
-        data: [617594, 181045, 153060, 106519, 105162, 95072],
-        backgroundColor: [
-          "rgba(255,99,132,0.6)",
-          "rgba(54,162,235,0.6)",
-          "rgba(255,206,86,0.6)",
-          "rgba(75,192,192,0.6)",
-          "rgba(153,102,255,0.6)",
-          "rgba(255,159,64,0.6)",
-          "rgba(255,99,132,0.6)",
-        ],
-      },
-    ],
   });
   return (
     <div className="chart">
-      <Bar
-        data={data}
-        options={{
-          maintainAspectRatio: false,
-        }}
-      />
+      <Bar data={data.chartData} options={{}} />
     </div>
   );
 };
